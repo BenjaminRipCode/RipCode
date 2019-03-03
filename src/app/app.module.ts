0e18543eea381bb0cafb53124cf1c817ae4322e9
Benjamin Ripoll <benjamin@ripoll.fr>
2019-03-03 22:19:32 +0100

    Fixed theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
-import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    MenuComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     AppRoutingModule,
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatToolbarModule, MatMenuModule } from '@angular/material';
 
 @NgModule({
